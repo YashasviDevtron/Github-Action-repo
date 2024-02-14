6305b0685f8903c7dbc543a60c76bd9dac9309cd
yashasvi.devtron <yashasvi.pareta@devtron.ai>
2024-02-14 17:11:49 +0530

    Please

--- create_release.sh
+++ create_release.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-
-
+set -ex
 read -p "Enter the tag for the release: " TAG
 read -p "Enter the revision (commitish) for the release: " REVISION
 read -p "Enter the path to the release notes file: " RELEASE_NOTES_PATH
@@ -17,20 +16,20 @@ if [ ! -d "$UPLOAD_ASSET" ]; then
     exit 1
 fi
 
-#if [ $(git tag -l "${TAG}") ]; then
- #  echo "Tag ${TAG} already exists"
-#else
+if [ $(git tag -l "${TAG}") ]; then
+   echo "Tag ${TAG} already exists"
+else
    git tag "${TAG}"
    echo "Tag ${TAG} created successfully."
    git push origin "${TAG}"
-#fi
+fi
 
-gh release create --target "$REVISION" --title "Release $TAG" -n "$RELEASE_NOTES_PATH/$RELEASE_FILE_NAME" "$TAG" --verify-tag
+gh release create --target "$REVISION" --title "Release $TAG" --notes-file "$RELEASE_NOTES_PATH/$RELEASE_FILE_NAME" "$TAG" --verify-tag
 echo "Release $TAG created successfully."
 
 cmd=""
 for file in "$UPLOAD_ASSET"/*;
   do
-    gh release upload "$TAG" "$file" --clobber
+    gh release upload "$TAG" "$file" 
   done
 
