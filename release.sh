5262c0668f67937816107cefd7f37ae639e5690a
YashasviDevtron <yashasvi.pareta@devtron.ai>
2024-02-16 18:12:56 +0530

    duhsaf

--- release.sh
+++ release.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -ex
-
 read -p "Enter the tag for the release: " TAG
 read -p "Enter the revision (commitish) for the release: " REVISION
 read -p "Enter the path to the release notes file: " RELEASE_NOTES_PATH
@@ -16,25 +15,37 @@ if [ ! -d "$UPLOAD_ASSET" ]; then
     exit 1
 fi
 
-# Check if the tag exists
-if [ $(git tag -l "${TAG}") ]; then
-   echo "Tag ${TAG} already exists"
-else
-   git tag "${TAG}"
-   echo "Tag ${TAG} created successfully."
-   git push origin "${TAG}"
-fi
+  # Check if the tag exists
+  gh release create \
+    --target "$REVISION" \
+    --title "Release $TAG" \
+    --notes-file "$RELEASE_NOTES_PATH" \
+    "$TAG" \
+    --verify-tag
 
-# Check if the release exists
-if gh release view "$TAG" &>/dev/null; then
-    echo "Release with tag '$TAG' already exists."
-else
-    gh release create --target "$REVISION" --title "Release $TAG" --notes-file "$RELEASE_NOTES_PATH" \
-    "$TAG"  --verify-tag
-    echo "Release $TAG created successfully."
+ if git tag -l "${TAG}"; then
+    echo "Tag ${TAG} already exists."
+  else
+    # Create the tag
+    git tag "${TAG}"
+    echo "Tag ${TAG} created successfully."
+    git push origin "${TAG}"
+    gh release create \
+    --target "$REVISION" \
+    --title "Release $TAG" \
+    --notes-file "$RELEASE_NOTES_PATH" \
+    "$TAG" \
 
-    # Upload assets
+  fi
+
+
+
+
+  if [[ -d "$UPLOAD_ASSET" ]]; then
     for file in "$UPLOAD_ASSET"/*; do
-        gh release upload "$TAG" "$file" 
+      gh release upload "$TAG" "$UPLOAD_ASSET" || echo "Error uploading asset '$UPLOAD_ASSET'."
     done
-fi
+  else
+    echo "No assets found in '$UPLOAD_ASSET' directory."
+  fi
+
